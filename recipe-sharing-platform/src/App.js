02223ed1e90384b5b0405364afb807ba8c6ec6b2
Delight Chinecherem <dlghtchenecherem@gmail.com>
2025-08-26 21:06:03 +0100

    React + Tailwind setup

---
+++ recipe-sharing-platform/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-600 text-white text-4xl font-bold">
+      Tailwind is working fine.
+    </div>
+  );
+}
+
+export default App;
