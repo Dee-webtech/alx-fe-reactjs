f1d3fe9dd2dc907fe35d18348d30413a52d4beed
Delight Chinecherem <dlghtchenecherem@gmail.com>
2025-09-02 16:31:45 +0100

    Add react-todo project inside alx-fe-reactjs repo

---
+++ react-todo/src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import TodoList from "./components/TodoList";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Todo App</h1>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
