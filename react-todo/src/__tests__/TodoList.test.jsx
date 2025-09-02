fd93ef3e9b3890380dac8e0a23a64a783e109783
Delight Chinecherem <dlghtchenecherem@gmail.com>
2025-09-02 17:39:41 +0100

    Add react-todo project

---
+++ react-todo/src/__tests__/TodoList.test.jsx
@@ -0,0 +1,29 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import TodoList from '../components/TodoList';
+
+test('renders initial todos', () => {
+  render(<TodoList />);
+  expect(screen.getByText(/Sample Todo 1/i)).toBeInTheDocument();
+});
+
+test('can add a new todo', () => {
+  render(<TodoList />);
+  const input = screen.getByPlaceholderText(/Add a new todo/i);
+  fireEvent.change(input, { target: { value: 'New Todo' } });
+  fireEvent.click(screen.getByText(/Add/i));
+  expect(screen.getByText(/New Todo/i)).toBeInTheDocument();
+});
+
+test('can toggle a todo', () => {
+  render(<TodoList />);
+  const todo = screen.getByText(/Sample Todo 1/i);
+  fireEvent.click(todo);
+  expect(todo).toHaveClass('completed');
+});
+
+test('can delete a todo', () => {
+  render(<TodoList />);
+  const deleteButton = screen.getByTestId('delete-Sample Todo 1');
+  fireEvent.click(deleteButton);
+  expect(screen.queryByText(/Sample Todo 1/i)).toBeNull();
+});
